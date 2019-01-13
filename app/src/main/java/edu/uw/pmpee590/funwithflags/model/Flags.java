4f30eb6bb4f7b0e3949d6820881aaf238b5aca1b
laura <arjonal@uw.edu>
2019-01-12 21:53:14 -0800

    idem

---
+++ app/src/main/java/edu/uw/pmpee590/funwithflags/model/Flags.java
@@ -0,0 +1,38 @@
+package edu.uw.pmpee590.funwithflags.model;
+
+public class Flags {
+
+
+    private int mFlagName; //Will contain the name of the country/flag
+    private int mFlagPict;
+    private int mFlagDetail;
+
+    public Flags(int flagPict, int flagName, int flagDetail) {
+        mFlagPict = flagPict;
+        mFlagName = flagName;
+        mFlagDetail = flagDetail;
+    }
+
+
+    public int getFlagPict() { return mFlagPict;  }
+
+    public void setFlagPict(int flagPict) { mFlagPict = flagPict; }
+
+    public int getFlagName() {
+        return mFlagName;
+    }
+
+    public void setFlagName(int flagName) {
+        mFlagName = flagName;
+    }
+
+    public int getFlagDetail() {
+        return mFlagDetail;
+    }
+
+    public void setFlagDetail(int flagDetail) {
+        mFlagDetail = flagDetail;
+    }
+
+
+}
